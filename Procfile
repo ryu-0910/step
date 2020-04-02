f4a406fad35f3ef6114b7e0053508207d70452cc
ryu0910 <ryuusuzuki243@gmail.com>
2020-04-02 09:03:28 +0900

    https強制

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 public/
+/Applications/MAMP/tmp/mysql/mysql.sock
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        // 本番環境に対応
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
